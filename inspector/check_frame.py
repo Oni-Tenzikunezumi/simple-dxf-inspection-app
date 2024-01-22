b5628ae1f85c207f434e978856b7f5308d52d376
S-Taichiii <ab20082@shibaura-it.ac.jp>
2024-01-22 19:19:07 +0900

    upload check_frame
    返り値などの仕様を合わせました

--- inspector/check_frame.py
+++ inspector/check_frame.py
@@ -1,69 +1,78 @@
 import ezdxf
 import tkinter as tk
 
+from ezdxf.document import Drawing
 from typing import Any
 from .check_base import CheckBase
 from .frame_extractor import Frame_extractor_result as FrameResult
+from .check_result import CheckResult
 from .draw_tool import DrawTool
 
 class CheckFrame(CheckBase):
     """枠線表示クラス"""
 
     # 検図項目の名前
-    inspect_name: str = "枠線表示"
+    inspect_name: str = "輪郭線表示"
 
     # 処理内容の説明
-    inspect_str: str = "図面上の枠線を強調する.\n"\
-        "表示項目"
+    inspect_str: str = "図面上の輪郭線を強調する."
     
     @staticmethod
-    def inspect_doc(doc: ezdxf.document.Drawing, **Option: tuple[Any]):
+    def inspect_doc(doc: Drawing, draw_doc: Drawing, **Option: tuple[Any]):
         """枠線表示"""
 
         fresult: FrameResult = Option["frameresult"]
 
+        # 結果
+        results = []
+        color = 1
+        num = 1
+
         # 枠線が存在しない場合
         if len(fresult.framePoint) == 0:
-            data = CheckFrame.__show_error(fresult)
-            document = doc
-            columns = ('error', 'reason')
+            res = CheckResult(
+                num = num,
+                checkType = CheckFrame.inspect_name,
+                error=True,
+                pos=None,
+                caption='輪郭線なし',
+                desc=fresult.message,
+                color=color
+            )
+            results.append(res)
 
-            return document, columns, data
-        
+            return draw_doc, results
+
         # 枠線は存在するがレイヤーがおかしい場合
         elif fresult.message == "枠線の構成するレイヤーがおかしいです":
-            DrawTool.Rectangle(doc, fresult.framePoint[0], fresult.framePoint[1], 1, 2)
+            DrawTool.Rectangle(draw_doc, fresult.framePoint[0], fresult.framePoint[1], color, 1.5)
 
-            data = CheckFrame.__show_error(fresult)
-            document = doc
-            columns = ('error', 'reason')
-            
-            return document, columns, data
+            res = CheckResult(
+                num = num,
+                checkType = CheckFrame.inspect_name,
+                error = True,
+                pos = None,
+                caption='輪郭線のレイヤーがおかしい',
+                desc = '輪郭線を構成するレイヤーが一つではありません',
+                color = color
+            )
+            results.append(res)
 
+            return draw_doc, results
 
-        DrawTool.Rectangle(doc, fresult.framePoint[0], fresult.framePoint[1], 1, 2)
+        # 枠線が存在する場合
+        DrawTool.Rectangle(draw_doc, fresult.framePoint[0], fresult.framePoint[1], color, 1.5)
+        res = CheckResult(
+            num = num,
+            checkType = CheckFrame.inspect_name,
+            error = False,
+            pos = None,
+            caption = '輪郭線が存在しました',
+            desc = '輪郭線が存在しました',
+            color = color
+        )
+        results.append(res)
 
-
-        # 図面の処理
-        data = []
-        # 表示する図面
-        document = doc
-
-        # 列名
-        columns = ('handle', 'center', 'radius')  # 列名の指定
-
-        return document, columns, data
+        return draw_doc, results
     
-
-    @staticmethod
-    def __show_error(fresult: FrameResult):
-        """枠線が存在しなかった場合のエラーを表示"""
-        datalabel: list[str] = ["error", "reason"]
-
-        ddict = {}
-        ddict[datalabel[0]] = "Not found frameline"
-        ddict[datalabel[1]] = fresult.message
-
-        return [ddict]
-
 
