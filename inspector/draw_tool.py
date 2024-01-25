9b6352f89bd3d1bb030040b87cddc2f14356113f
Y.Kuronuma <78721329+Oni-Tenzikunezumi@users.noreply.github.com>
2024-01-25 21:04:37 +0900

    Update draw_tool.py
    レイアウト変更．

--- inspector/draw_tool.py
+++ inspector/draw_tool.py
@@ -15,9 +15,9 @@ from ezdxf.document import Drawing
 from ezdxf.entities import MText, Text
 
 class DrawTool:
-    
+
     DefaultLayer = "inspection"
-    
+
     @staticmethod
     def Rectangle( doc: Drawing, p1, p2, color, width=1, layer=None):
         '''
@@ -27,19 +27,19 @@ class DrawTool:
         # 画層
         if layer is None:
             layer = DrawTool.DefaultLayer
-        
+
         # 座標
         xmin = min( p1[0], p2[0] )
         xmax = max( p1[0], p2[0] )
         ymin = min( p1[1], p2[1] )
         ymax = max( p1[1], p2[1] )
         points = [ (xmin, ymax), (xmax, ymax), (xmax, ymin), (xmin, ymin)]
-        
+
         msp = doc.modelspace()
         att = {'layer': layer, 'color' : color, 'linetype' : 'Continuous','const_width' : width}
         msp.add_lwpolyline( points, format='xy', close=True, dxfattribs= att)
-        
-    @staticmethod        
+
+    @staticmethod
     def Line( doc: Drawing, p1, p2, color, width=1, layer=None):
         '''
         直線を描画
@@ -47,19 +47,19 @@ class DrawTool:
         # 画層
         if layer is None:
             layer = DrawTool.DefaultLayer
-            
-            
+
+
         msp = doc.modelspace()
         att = {'layer': layer, 'color' : color, 'linetype' : 'Continuous','const_width' : width}
         msp.add_lwpolyline( (p1, p2), format='xy', close=False, dxfattribs=att)
-            
-        
+
+
     @staticmethod
     def Arrow( doc: Drawing, head, tail, color, head_size=5, line_width=1, layer=None):
         '''
         矢印を描画
         '''
-        
+
         # 画層
         if layer is None:
             layer = DrawTool.DefaultLayer
@@ -68,64 +68,64 @@ class DrawTool:
         dx = tail[0] - head[0]
         dy = tail[1] - head[1]
         angle = math.atan2(dy, dx)
-        
+
         # ヘッドの座標
         y = math.sin(math.pi * 20/180) # sin(20deg)
         points = [ (0,0), ( head_size, head_size*y), ( head_size, -head_size*y) ]
-        
+
         # ヘッド座標変換
         sina = math.sin(angle)
         cosa = math.cos(angle)
         trans = lambda p : (p[0]*cosa-p[1]*sina + head[0], p[0]*sina+p[1]*cosa + head[1])
         points = list( map( trans, points ) )
-        
+
         # ヘッド部分の描画
         msp = doc.modelspace()
         hatch = msp.add_hatch(color=color)
         hatch.paths.add_polyline_path( points, is_closed=True)
-        
+
         # 線部分の描画
         p = ( (points[1][0]+points[2][0])/2, (points[1][1]+points[2][1])/2 )
         DrawTool.Line(doc, p, tail, color=color, width=line_width, layer=layer)
-        
+
     @staticmethod
     def Circle( doc: Drawing, cent, radius, color, width=1, layer=None ):
         '''
         円を描画
         '''
-        
+
         # 画層
         if layer is None:
             layer = DrawTool.DefaultLayer
-        
+
         # LWPolyLineで描く（ add_circle だと太さの変更が難しいため)
         n = 24
         pts = []
         for i in range(n):
             p = ( cent[0]+radius * cos(2*math.pi*i/n), cent[1]+radius * sin(2*math.pi*i/n) )
             pts.append(p)
-            
-        
+
+
         msp = doc.modelspace()
         att = {'layer': layer, 'color' : color, 'linetype' : 'Continuous','const_width' : width}
         msp.add_lwpolyline( pts, format='xy', close=True, dxfattribs=att)
-        
+
     def Arc( doc: Drawing, cent, radius, stdeg, eddeg, color, width=1, layer=None ):
         '''
         円弧を描画
         '''
-        
+
         # 画層
         if layer is None:
             layer = DrawTool.DefaultLayer
-        
+
         # degree -> rad
         if eddeg < stdeg:
             eddeg += 360
         st = stdeg * math.pi / 180
         ed = eddeg * math.pi / 180
         ag = ed - st
-        
+
         # LWPolyLine
         n = int(ag / math.pi * 12) + 1  # 180度で12辺程度
         pts = []
@@ -133,7 +133,7 @@ class DrawTool:
             theta = st+i*ag/n
             p = ( cent[0]+radius*cos(theta), cent[1]+radius*sin(theta))
             pts.append(p)
-            
+
         msp = doc.modelspace()
         att = {'layer': layer, 'color' : color, 'linetype' : 'Continuous','const_width' : width}
         msp.add_lwpolyline( pts, format='xy', close=False, dxfattribs=att)
@@ -141,9 +141,9 @@ class DrawTool:
     @staticmethod
     def Text( doc: Drawing, txt, pos, color, height=15, layer=None ):
         '''
-        テキストを描画 
+        テキストを描画
         '''
-        
+
         # 画層
         if layer is None:
             layer = DrawTool.DefaultLayer
@@ -167,7 +167,7 @@ class DrawTool:
         newdoc = ezdxf.read( stream )
         stream.close()
         return newdoc
-    
+
     @staticmethod
     def ResolveFont( doc : Drawing ) -> Drawing:
         '''
@@ -176,15 +176,15 @@ class DrawTool:
         # style 中の iso.shx フォント
         for s in doc.styles:
             s.dxf.font = s.dxf.font.replace( 'iso.shx', 'isocp')
-            
+
         # MS PGothic
         for e in doc.entities:
-            if type(e) is MText or type(e) is Text:
+            if type(e) is MText:
                 e.text = e.text.replace( 'MS PGothic', 'MS Gothic' )
-                
+
         return doc
-        
-        
+
+
 # テスト
 if __name__ == '__main__':
     from ezdxf.addons.drawing import RenderContext, Frontend
@@ -195,7 +195,7 @@ if __name__ == '__main__':
 
 
     doc = ezdxf.new("R2018", setup=True)
-    
+
     DrawTool.Rectangle(doc, (0,0), (400,300), color= 2, width=1)
     DrawTool.Line(doc, (0,0), (200, 100), color=3, width=4 )
     DrawTool.Text(doc, "テスト", (0,0), color=1, height=14 )
@@ -204,7 +204,7 @@ if __name__ == '__main__':
 
     aaa = DrawTool.CopyDoc(doc)
     msp = doc.modelspace()
-    
+
     fig = plt.figure()
     ax = fig.add_axes([0,0,20,20])
     ctx = RenderContext(doc)
